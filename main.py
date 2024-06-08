9560fb4dc935f16f1db09365e1e058e082e03960
mpiele <mpiele08@gmail.com>
2024-06-08 15:37:09 +0300

    Fixed bugs, added printing and commenting

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import re
+
 print("WELCOME,")
 print("HOLY SCRIPT VERSION 1.0")
 
@@ -9,6 +11,38 @@ def getCodeFile():
         print("ERROR: No file found")
         getCodeFile()
     else:
-        return code_file.read()
+        code_file_content = code_file.read().splitlines()
 
-getCodeFile()
+        return code_file_content
+
+def runCode():
+    tmp_code_file_content = getCodeFile()
+    
+    for code_line in tmp_code_file_content:
+        code_line = code_line.strip()
+
+        if code_line[0] == "p":
+            final_print_text = ""
+            is_bracket = False
+
+            for character in code_line:
+                if character == "(":
+                    is_bracket = True
+                elif character == ")":
+                    is_bracket == False
+
+                if is_bracket == True:
+                    final_print_text += character
+            
+            final_print_text = final_print_text[1:-1]
+            print(final_print_text)
+
+        elif code_line[0] == "!":
+            print("COMMENT LINE")
+            
+            
+        else:
+            print("NOT RECOGNIZED")
+
+runCode()
+
