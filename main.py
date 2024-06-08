907543d7944e31a0f59f4469bb0e478230460931
mpiele <mpiele08@gmail.com>
2024-06-08 14:14:28 +0300

    Added a way to open the code file

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 print("WELCOME,")
 print("HOLY SCRIPT VERSION 1.0")
 
+def getCodeFile():
+    code_file_name = input("Name of your .txt code file:")
+    try:
+        code_file = open(code_file_name, "r")
+    except:
+        print("ERROR: No file found")
+        getCodeFile()
+    else:
+        return code_file.read()
+
+getCodeFile()
